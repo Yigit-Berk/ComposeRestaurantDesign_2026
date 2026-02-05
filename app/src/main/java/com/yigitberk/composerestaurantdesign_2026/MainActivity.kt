1f10aa6c1905817abc37b2e8158de30fcefebfaf
Yigit-Berk <yigithizarci@hotmail.com>
2026-02-05 17:43:21 +0300

    Start Screen UI Codes

--- app/src/main/java/com/yigitberk/composerestaurantdesign_2026/MainActivity.kt
+++ app/src/main/java/com/yigitberk/composerestaurantdesign_2026/MainActivity.kt
@@ -4,38 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.yigitberk.composerestaurantdesign_2026.screens.StartScreen
 import com.yigitberk.composerestaurantdesign_2026.ui.theme.ComposeRestaurantDesign_2026Theme
 
 class MainActivity : ComponentActivity() {
@@ -44,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeRestaurantDesign_2026Theme {
-
+                StartScreen()
             }
         }
     }
---
+++ app/src/main/java/com/yigitberk/composerestaurantdesign_2026/screens/start.kt
@@ -0,0 +1,94 @@
+package com.yigitberk.composerestaurantdesign_2026.screens
+
+import android.R
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yigitberk.composerestaurantdesign_2026.ui.theme.ComposeRestaurantDesign_2026Theme
+
+
+@Composable
+fun StartScreen(){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.background(Color.White)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "You are a",
+            style = TextStyle(
+                fontSize = 34.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+            ,textAlign = TextAlign.Start
+        )
+        Spacer(Modifier.height(20.dp))
+        Button(modifier = Modifier.fillMaxWidth()
+            .height(48.dp)
+            .padding(horizontal = 30.dp),
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(Color(100,252,217)),
+            shape = RoundedCornerShape(12.dp)// for corner radius
+        ) {
+            Text(
+                text = "Restaurant",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 18.sp
+                )
+            )
+        }
+        Spacer(Modifier.height(20.dp))
+        Button(modifier = Modifier.fillMaxWidth()
+            .height(48.dp)
+            .padding(horizontal = 30.dp),
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(Color(100,252,217)),
+            shape = RoundedCornerShape(12.dp)// for corner radius
+        ) {
+            Text(
+                text = "Customer",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 18.sp
+                )
+            )
+        }
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StartPreview() {
+    ComposeRestaurantDesign_2026Theme {
+        StartScreen()
+    }
+}
